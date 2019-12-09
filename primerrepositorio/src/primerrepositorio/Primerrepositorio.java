8744a8045c525ac469704a6487d60f058228d9c0
PC-05 <PC-05@PC-05>
2019-12-09 17:51:30 -0300

    mi primer conexion en github

---
+++ primerrepositorio/src/primerrepositorio/Primerrepositorio.java
@@ -0,0 +1,17 @@
+
+package primerrepositorio;
+
+
+public class Primerrepositorio {
+
+    
+    
+    public static void main(String[] args) {
+       
+        System.out.println("mi primer conexion con github");
+        
+        
+        
+    }
+    
+}
